7da534a5428953d372cc7d2108c2b3f33ffa3e4b
Jonas Friederich <33685239+jo-chr@users.noreply.github.com>
2023-04-21 09:31:35 +0200

    rename dir

--- one_server_example.py
+++ one_server_example.py
@@ -1,7 +1,7 @@
-from spn_simulator.spn import *
-from spn_simulator.spn_simulate import simulate
-from spn_simulator.spn_io import print_petri_net
-from spn_simulator.spn_visualization import *
+from components.spn import *
+from components.spn_simulate import simulate
+from components.spn_io import print_petri_net
+from components.spn_visualization import *
 
 spn = SPN()
 
@@ -28,7 +28,7 @@ spn.add_output_arc(t2,p2)
 spn.add_input_arc(p2,t3)
 spn.add_inhibitor_arc(t2,p2)
 
-simulate(spn, max_time = 100, verbosity = 3, protocol = True)
+#simulate(spn, max_time = 100, verbosity = 3, protocol = True)
 
 #print_petri_net(spn)
 draw_spn(spn, show=False)
