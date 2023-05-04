e3dfb91c2d84627e24946ca3f3a6817630a1fc6e
Jonas Friederich <33685239+jo-chr@users.noreply.github.com>
2023-05-04 14:34:49 +0200

    Update one_server_example.py

--- one_server_example.py
+++ one_server_example.py
@@ -9,11 +9,11 @@ p1 = Place(label="Queue", n_tokens=0)
 p2 = Place(label="Server", n_tokens=0)
 
 t1 = Transition(label="TArrive", t_type="T")
-t1.set_distribution(distribution="EXP", parameter1=1.0, parameter2=0.0)
+t1.set_distribution(distribution="expon", a=0.0, b=1.0/1.0)
 t2 = Transition(label="TApproach", t_type="I")
 t2.set_weight(weight=1)
 t3 = Transition(label="TService", t_type="T")
-t3.set_distribution(distribution="EXP", parameter1=0.9, parameter2=0.0)
+t3.set_distribution(distribution="expon", a=0.0, b=1.0/1.1)
 
 spn.add_place(p1)
 spn.add_place(p2)
@@ -28,7 +28,7 @@ spn.add_output_arc(t2,p2)
 spn.add_input_arc(p2,t3)
 spn.add_inhibitor_arc(t2,p2)
 
-simulate(spn, max_time = 100, verbosity = 2, protocol = True)
+#simulate(spn, max_time = 100, verbosity = 2, protocol = True)
 
 #print_petri_net(spn)
 draw_spn(spn, show=False)
