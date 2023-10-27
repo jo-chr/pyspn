773576dbbe397eaf2bb97cd711d739c781a667e2
Jonas Friederich <33685239+jo-chr@users.noreply.github.com>
2023-10-27 12:29:33 +0200

    Update two_server_with_guard&multiplicity.py

--- examples/two_server_with_guard&multiplicity.py
+++ examples/two_server_with_guard&multiplicity.py
@@ -6,6 +6,8 @@ from components.spn_simulate import simulate
 from components.spn_io import print_petri_net
 from components.spn_visualization import *
 
+#from . import *
+
 spn = SPN()
 
 p1 = Place("Queue",0)
@@ -13,7 +15,7 @@ p2 = Place("Server 1",0)
 p3 = Place("Server 2",0)
 
 t1 = Transition("Arrive","T")
-t1.set_distribution("expon", a=0.0, b=1.0/1.8)
+t1.set_distribution("expon", a=0.0, b=1.0/3.0)
 t2 = Transition("Approach Server 1","I")
 t2.set_weight(0.5)
 t3 = Transition("Approach Server 2","I")
@@ -24,9 +26,9 @@ def guard_t3():
 t3.set_weight(0.5)
 t3.set_guard_function(guard_t3)
 t4 = Transition("Service Server 1","T")
-t4.set_distribution("expon", a=0.0, b=1.0/0.9)
+t4.set_distribution("norm", a=0.0, b=1.0)
 t5 = Transition("Service Server 2","T")
-t5.set_distribution("expon", a=0.0, b=1.0/0.5)
+t5.set_distribution("norm", a=0.0, b=1.0)
 
 spn.add_place(p1)
 spn.add_place(p2)
@@ -49,7 +51,7 @@ spn.add_input_arc(p3,t5,multiplicity=2)
 spn.add_inhibitor_arc(t3,p3,multiplicity=2)
 
 
-simulate(spn, max_time = 100, verbosity = 0, protocol = True)
+simulate(spn, max_time = 100, verbosity = 2, protocol = True)
 
 #print_petri_net(spn)
 draw_spn(spn, show=False,rankdir="TB")
