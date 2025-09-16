b4a4d16843a81d4116574f807fe51a1049abb7ba
Atieh Khodadadi <46083261+atikh@users.noreply.github.com>
2025-09-16 15:43:57 +0200

    Update spn.py

--- components/spn.py
+++ components/spn.py
@@ -1,26 +1,30 @@
 from __future__ import annotations
 import uuid
+
+
 class SPN(object):
 
     def __init__(self):
         self.places = []
         self.transitions = []
-        
-    def add_place(self, place: Place):            
+
+    def add_place(self, place: Place):
         self.places.append(place)
 
     def add_transition(self, transition: Transition):
         self.transitions.append(transition)
 
-    def add_input_arc(self, place: Place, transition: Transition, multiplicity = 1):
+    def add_input_arc(self, place: Place, transition: Transition, multiplicity=1):
         arc = InputArc()
         arc.from_place = place
         arc.to_transition = transition
         arc.multiplicity = multiplicity
         transition.input_arcs.append(arc)
         place.input_arcs.append(arc)
+        # Auto-set Join: more than one distinct input place -> Join
+        transition.Join = len(transition.input_arcs) > 1
 
-    def add_inhibitor_arc(self, transition: Transition, place: Place, multiplicity = 1):
+    def add_inhibitor_arc(self, transition: Transition, place: Place, multiplicity=1):
         arc = InhibitorArc()
         arc.to_transition = transition
         arc.from_place = place
@@ -28,13 +32,15 @@ class SPN(object):
         transition.inhibitor_arcs.append(arc)
         place.inhibitor_arcs.append(arc)
 
-    def add_output_arc(self, transition: Transition, place: Place, multiplicity = 1):
+    def add_output_arc(self, transition: Transition, place: Place, multiplicity=1):
         arc = OutputArc()
         arc.from_transition = transition
         arc.to_place = place
         arc.multiplicity = multiplicity
         transition.output_arcs.append(arc)
         place.output_arcs.append(arc)
+        # Auto-set Fork: more than one distinct output place -> Fork
+        transition.Fork = len(transition.output_arcs) > 1
 
     '''
     def get_current_marking(self):
@@ -44,13 +50,15 @@ class SPN(object):
             marking[place]=place.n_tokens
             return marking
     '''
+
     def get_place_by_label(self, label: str):
         try:
             place: Place
             for place in self.places:
                 if place.label == label:
                     return place
-        except: print("No place found with specified label.")
+        except:
+            print("No place found with specified label.")
 
     def get_transition_by_label(self, label: str):
         try:
@@ -58,7 +66,14 @@ class SPN(object):
             for transition in self.transitions:
                 if transition.label == label:
                     return transition
-        except: print("No transition found with specified label.")
+        except:
+            print("No transition found with specified label.")
+
+    def refresh_join_fork(self):
+        """Recompute Join/Fork flags for all transitions based on arc counts."""
+        for t in self.transitions:
+            t.Join = len(t.input_arcs) > 1
+            t.Fork = len(t.output_arcs) > 1
 
     def get_arrival_transitions(self):
         try:
@@ -68,9 +83,9 @@ class SPN(object):
                 if transition.input_arcs == []:
                     arrival_transitions.append(transition)
             return arrival_transitions
-        except: print("Failed to find arrival transitions.")
+        except:
+            print("Failed to find arrival transitions.")
 
-        
 
 class Place:
 
@@ -102,6 +117,7 @@ class Place:
             # Remove tokens if the number has decreased
             self.tokens = self.tokens[:n_tokens]
 
+
 class Transition(object):
 
     def __init__(self, label: str, t_type: str, Join=0, Fork=0):
@@ -113,12 +129,13 @@ class Transition(object):
             self.time_unit = None
         elif self.t_type == "I":
             self.weight = 1
-        else: raise Exception("Not a valid transition type.")
+        else:
+            raise Exception("Not a valid transition type.")
 
-        self.handicap = 1 #Handicap for experiments with new configurations; Handicap will influence firing times of transition; <1 -> transition fill fire faster; >1 -> transition will fire slower
+        self.handicap = 1  # Handicap for experiments with new configurations; Handicap will influence firing times of transition; <1 -> transition fill fire faster; >1 -> transition will fire slower
         self.handicap_type = None
         self.allow_reset = False
-        self.reset_threshold = 0 #Threshold; can be used e.g., for testing preventive maintenance with equipment failure transitions
+        self.reset_threshold = 0  # Threshold; can be used e.g., for testing preventive maintenance with equipment failure transitions
         self.reset_time = 0
 
         self.guard_function = None
@@ -141,18 +158,20 @@ class Transition(object):
 
         self.counter = 0
         self.Join = Join
-        self.Fork=Fork
+        self.Fork = Fork
 
-    def set_distribution(self, distribution, a=0.0, b=0.0, c=0.0, d=0.0, time_unit:str = None):
+    def set_distribution(self, distribution, a=0.0, b=0.0, c=0.0, d=0.0, time_unit: str = None):
         if self.t_type == "T":
-            self.distribution = {distribution:{"a":a,"b":b,"c":c,"d":d}}
+            self.distribution = {distribution: {"a": a, "b": b, "c": c, "d": d}}
             self.time_unit = time_unit
-        else: raise Exception("Can not set distribution for immediate transition.")
-    
+        else:
+            raise Exception("Can not set distribution for immediate transition.")
+
     def set_weight(self, weight: float):
         if self.t_type == "I":
             self.weight = weight
-        else: raise Exception("Can not set weight for timed transition.")
+        else:
+            raise Exception("Can not set weight for timed transition.")
 
     def set_guard_function(self, guard_function):
         self.guard_function = guard_function
@@ -160,17 +179,20 @@ class Transition(object):
     def set_memory_policy(self, type):
         self.memory_policy = type
 
+
 class InputArc(object):
     def __init__(self):
         self.to_transition = None
         self.from_place = None
         self.multiplicity = 0
 
+
 class InhibitorArc(object):
     def __init__(self):
         self.to_transition = None
         self.from_place = None
         self.multiplicity = 0
+
 
 class OutputArc(object):
     def __init__(self):
